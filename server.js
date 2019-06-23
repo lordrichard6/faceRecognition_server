1bf661b1994f2f4470f11bffdb5de0a2ee983daf
lordrichard <pauloreizinho@hotmail.com>
2019-06-23 17:32:32 +0200

    add ssl syntax

--- server.js
+++ server.js
@@ -20,10 +20,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : 'postgresql-reticulated-42326',
-      user : 'reizinho',
-      password : '',
-      database : 'smart-brain'
+      host : process.env.DATABASE_URL,
+      ssl: true
     }
   });
 
